2cf9e38f4ee53c26b688ba159a96c45c10856926
mertyalcin <82022374+mertyalcin-code@users.noreply.github.com>
2022-01-23 21:54:46 +0300

    heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/rent-acar'));
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/rent-acar/'}),
-);
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/rent-acar/index.html'));
+});
 
 ngApp.listen(process.env.PORT || 8080);
